1d81a0465d365c30146a028ae52b3fff84d9500d
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2025-01-12 15:51:40 +0000

    Update compiler.h

--- src/compiler.h
+++ src/compiler.h
@@ -30,131 +30,107 @@ int vasm_len(const char *str) {
 	return len;
 }
 
-char vasm_strcmp(const char *str1, const char *str2, int len) {
-	for (int ch = 0; ch < len; ch++) {
-		if (str1[ch] != str2[ch]) {
+char vasm_strcmp(const char *str1, const char *str2, int len, int b) {
+	for (int ch = 0; ch < (len+b); ch++) {
+		if (str1[ch+b] != str2[ch+b]) {
 			return 0;
 		}
 	}
 	return 1;
 }
 
-Ins first(const char *str, int pos) {
-	Ins ins_ret;
-	ins_ret.fpos = pos;
-	for (int i = pos; str[i] != ' ' && i < INS_SIZE; i++) {
-		ins_ret.inst[i] = str[i];
-		ins_ret.fpos++;
-	}
-	return ins_ret;
-}
 
-Ins split(const char *str, int who) {
-	Ins ret;
-	int pos = 0;
-	for (int a = 0; a < who; a++) {
-		ret = first(str, pos);
-		pos = ret.fpos + 2;
-	}
-	return ret;
-}
 
 int asemble(const char *line) {
 	int opcode = -1;
 	int reg_idx = -1;
 	int value = -1;
 	int in = -1;
-	Ins pt;
-	pt = split(line, 0);
-	if (vasm_strcmp(pt.inst, "MOV", 3)) {
+	if (vasm_strcmp(line, "MOV", 3, 0)) {
 		opcode = 1;
-	} else if (vasm_strcmp(pt.inst, "ADD", 3)) {
+	} else if (vasm_strcmp(line, "ADD", 3, 0)) {
 		opcode = 2;
-        } else if (vasm_strcmp(pt.inst, "SUB", 3)) {
+        } else if (vasm_strcmp(line, "SUB", 3, 0)) {
 		opcode = 3;
-        } else if (vasm_strcmp(pt.inst, "MUL", 3)) {
+        } else if (vasm_strcmp(line, "MUL", 3, 0)) {
 		opcode = 4;
-        } else if (vasm_strcmp(pt.inst, "DIV", 3)) {
+        } else if (vasm_strcmp(line, "DIV", 3, 0)) {
 		opcode = 5;
-        } else if (vasm_strcmp(pt.inst, "JNQ", 3)) {
+        } else if (vasm_strcmp(line, "JNQ", 3, 0)) {
 		opcode = 7;
 		value = 4;
-        } else if (vasm_strcmp(pt.inst, "LOAD", 4)) {
+        } else if (vasm_strcmp(line, "LOA", 3, 0)) {
 		opcode = 8;
-        } else if (vasm_strcmp(pt.inst, "STORE", 5)) {
+        } else if (vasm_strcmp(line, "STR", 3, 0)) {
 		opcode = 6;
-        } else if (vasm_strcmp(pt.inst, "JLT", 3)) {
+        } else if (vasm_strcmp(line, "JLT", 3, 0)) {
 		opcode = 7;
 		value = 1;
-        } else if (vasm_strcmp(pt.inst, "JGT", 3)) {
+        } else if (vasm_strcmp(line, "JGT", 3, 0)) {
 		opcode = 7;
 		value = 3;
-        } else if (vasm_strcmp(pt.inst, "JQT", 3)) {
+        } else if (vasm_strcmp(line, "JQT", 3, 0)) {
 		opcode = 7;
 		value = 2;
-        } else if (vasm_strcmp(pt.inst, "JMP", 3)) {
+        } else if (vasm_strcmp(line, "JMP", 3, 0)) {
 		opcode = 7;
 		value = 5;
 	}
-	if (opcode < 0) {
+	if (opcode < 0) { // MOV AX NUM 5
 		return -1;
 	}
-	pt = split(line, 1);
-        if (vasm_strcmp(pt.inst, "AX", 2)) {
+        if (vasm_strcmp(line, "AX", 2, 4)) {
 		reg_idx = 0;
-        } else if (vasm_strcmp(pt.inst, "BX", 2)) {
+        } else if (vasm_strcmp(line, "BX", 2, 4)) {
 		reg_idx = 1;
-        } else if (vasm_strcmp(pt.inst, "CX", 2)) {
+        } else if (vasm_strcmp(line, "CX", 2, 4)) {
 		reg_idx = 2;
-        } else if (vasm_strcmp(pt.inst, "DX", 2)) {
+        } else if (vasm_strcmp(line, "DX", 2, 4)) {
 		reg_idx = 3;
-        } else if (vasm_strcmp(pt.inst, "EX", 2)) {
+        } else if (vasm_strcmp(line, "EX", 2, 4)) {
 		reg_idx = 4;
-        } else if (vasm_strcmp(pt.inst, "FX", 2)) {
+        } else if (vasm_strcmp(line, "FX", 2, 4)) {
 		reg_idx = 5;
-        } else if (vasm_strcmp(pt.inst, "GX", 2)) {
+        } else if (vasm_strcmp(line, "GX", 2, 4)) {
 		reg_idx = 6;
-        } else if (vasm_strcmp(pt.inst, "HX", 2)) {
+        } else if (vasm_strcmp(line, "HX", 2, 4)) {
 		reg_idx = 7;
-        } else if (vasm_strcmp(pt.inst, "IX", 2)) {
+        } else if (vasm_strcmp(line, "IX", 2, 4)) {
 		reg_idx = 8;
 	} if (reg_idx < 0) {
-		return -1;
+		return -2;
 	}
-	pt = split(line, 2);
 	if (value < 0) {
-	        if (vasm_strcmp(pt.inst, "NUM", 3)) {
+	        if (vasm_strcmp(line, "NUM", 3, 6)) {
 			value = 0;
-	        } else if (vasm_strcmp(pt.inst, "REG", 3)) {
+	        } else if (vasm_strcmp(line, "REG", 3, 6)) {
 			value = 1;
 		}
 	} if (value < 0) {
-		return -1;
-	pt = split(line, 3);
+		return -3;
 	} if (opcode == 7 || value == 1) {
-	        if (vasm_strcmp(pt.inst, "AX", 2)) {
+	        if (vasm_strcmp(line, "AX", 2, 9)) {
         	        in = 0;
-	        } else if (vasm_strcmp(pt.inst, "BX", 2)) {
+	        } else if (vasm_strcmp(line, "BX", 2, 9)) {
         	        in = 1;
-	        } else if (vasm_strcmp(pt.inst, "CX", 2)) {
+	        } else if (vasm_strcmp(line, "CX", 2, 9)) {
         	        in = 2;
-	        } else if (vasm_strcmp(pt.inst, "DX", 2)) {
+	        } else if (vasm_strcmp(line, "DX", 2, 9)) {
         	        in = 3;
-	        } else if (vasm_strcmp(pt.inst, "EX", 2)) {
+	        } else if (vasm_strcmp(line, "EX", 2, 9)) {
         	        in = 4;
-	        } else if (vasm_strcmp(pt.inst, "FX", 2)) {
+	        } else if (vasm_strcmp(line, "FX", 2, 9)) {
 	                in = 5;
-	        } else if (vasm_strcmp(pt.inst, "GX", 2)) {
+	        } else if (vasm_strcmp(line, "GX", 2, 9)) {
         	        in = 6;
-	        } else if (vasm_strcmp(pt.inst, "HX", 2)) {
+	        } else if (vasm_strcmp(line, "HX", 2, 9)) {
 	                in = 7;
-	        } else if (vasm_strcmp(pt.inst, "IX", 2)) {
+	        } else if (vasm_strcmp(line, "IX", 2, 9)) {
 	                in = 8;
 	        }
 	} else {
-		in = pt.inst[0] - '0';
+		in = pt.inst[9] - '0';
 	}
-	int bytecode = 10000;
 	int bytecode = 10000 + in * 1000 + value * 100 + reg_idx * 10 + opcode;
 	return bytecode;
 }
