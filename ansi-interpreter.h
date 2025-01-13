7adaf04371e92e438bc3e66495297226e43467f1
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2025-01-13 10:28:18 +0100

    Create ansi-interpreter.h

---
+++ ansi-interpreter.h
@@ -0,0 +1,18 @@
+#include "vasm.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LEN 20
+#define MAX_INS 65536
+
+void compile_run(const char *file) {
+  FILE *f = fopen(file, "r");
+  char buffer[MAX_LEN];
+  int instructions[MAX_INS];
+  int time = 0;
+  while (fgets(buffer, MAX_LEN, f)) {
+    strtok(buffer, '\n');
+    instructions[time] = asemble(buffer);
+    time++;
+  }
+}
