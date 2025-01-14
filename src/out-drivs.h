5d40df88786316351d58adb13e07b026a95004fc
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2025-01-14 15:24:46 +0000

    Update out-drivs.h

--- src/out-drivs.h
+++ src/out-drivs.h
@@ -1,24 +1,21 @@
 #ifdef MUNIX_KERNEL
+
 void pch(char ch, int x, int y) {
-  putchar(ch, pos);
+  putchar(ch, x+y*80);
 }
+
 #endif
-#ifdef WINDOWS
-#include <conio.h>
+
+#ifndef MUNIX_KERNEL
 #include <stdio.h>
-void pch(char ch, int x, int y) {
-  gotoxy(int x, int y);ç
-  putchar(ch);
-}
-#endif
-#ifdef LINUX
-#include <stdio.h>
+
 void gotoxy(int x,int y)    
 {
     printf("%c[%d;%df",0x1B,y,x);
 }
 void pch(char ch, int x, int y) {
-  gotoxy(int x, int y);ç
+  gotoxy(int x, int y);
   putchar(ch);
 }
+
 #endif
