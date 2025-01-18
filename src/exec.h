868a17021cb4ab22862ff02f4b63cf6fdc71b292
Guille <guilleleiratemes@gmail.com>
2025-01-18 15:13:50 +0000

    upgrading things for better abstraction

--- src/exec.h
+++ src/exec.h
@@ -6,63 +6,61 @@ typedef struct {
     int ram[512];
 } VMachine;
 
-VMachine mach;
-
-void init_mach() {
-    mach.counter = 0;
+void init_mach(VMachine *mach) {
+    mach->counter = 0;
     for (int i = 0; i < 9; i++) {
-        mach.regs[i] = 0;
+        mach->regs[i] = 0;
     }
     for (int i = 0; i < 512; i++) {
-        mach.ram[i] = 0;
+        mach->ram[i] = 0;
     }
 }
 
-void exec(int ins) {
+void exec(int ins, VMachine *mach) {
     int opcode = ins % 10;
     ins /= 10;
     int reg_idx = ins % 10;
     int value = (ins / 10) % 10;
-    int *outreg = &mach.regs[reg_idx];
-    int in = (ins / 10*10) % 10;
+    int *outreg = &mach->regs[reg_idx];
+    int in = (ins / 100) % 10;
 
     if (opcode == 1) {
         if (value == 0) {
             *outreg = in;
         } else {
-            *outreg = mach.regs[in];
+            *outreg = mach->regs[in];
         }
     } else if (opcode == 2) {
         if (value == 0) {
             *outreg += in;
         } else {
-            *outreg += mach.regs[in];
+            *outreg += mach->regs[in];
         }
     } else if (opcode == 3) {
         if (value == 0) {
             *outreg -= in;
         } else {
-            *outreg -= mach.regs[in];
+            *outreg -= mach->regs[in];
         }
     } else if (opcode == 4) {
         if (value == 0) {
             *outreg *= in;
         } else {
-            *outreg *= mach.regs[in];
+            *outreg *= mach->regs[in];
         }
     } else if (opcode == 5) {
         if (value == 0) {
             *outreg /= in;
         } else {
-            *outreg /= mach.regs[in];
+            *outreg /= mach->regs[in];
         }
     } else if (opcode == 6) {
-        mach.ram[mach.regs[reg_idx]] = mach.regs[in];
+        mach->ram[mach->regs[reg_idx]] = mach->regs[in];
     } else if (opcode == 7) {
         int condition1 = value;
-        int jump_target = mach.regs[in];
+        int jump_target = mach->regs[in];
 
-        int reg_val = mach.regs[reg_idx];
+        int reg_val = mach->regs[reg_idx];
         int condition_met = 0;
 
         if (condition1 == 1 && reg_val < 0) {
@@ -81,13 +79,13 @@ void exec(int ins) {
         }
 
         if (condition_met) {
-            mach.counter = jump_target;
-        } else if (opcode == 8) {
-            *outreg = mach.ram[mach.regs[in]];
-        } else if (opcode == 9) {
-            if (value == 0) {
-                pch(mach.regs[reg_idx], mach.regs[in] / 80, mach.regs[in] % 80);
-            }
+            mach->counter = jump_target;
+	}
+    } else if (opcode == 8) {
+        *outreg = mach->ram[mach->regs[in]];
+    } else if (opcode == 9) {
+        if (value == 0) {
+            pch(mach->regs[reg_idx], mach->regs[in] / 80, mach->regs[in] % 80);
         }
     }
 }
